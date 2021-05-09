5a777b8573c4bd5e5ef604ed7a01d77f4c1362b5
Karthik Muruganantham <karthik.muruganantham@syncfusion.com>
2021-05-09 15:33:16 +0530

    Changes for 3tier architecture for code first approach to fetch data

---
+++ Controllers/OrderDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp1.Models;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace WebApp1.Controllers
+{
+    public class OrderDataController : ApiController
+    {
+        [System.Web.Http.HttpHead]
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.HttpGet]
+        // Api to get all customer order count.
+        [System.Web.Http.Route("api/get-orders-data")]
+        public ApiResponse GetCustomerCount()
+        {
+            ApiResponse response = new ApiResponse()
+            {
+                Status = true,
+                Data = null,
+                Message = ""
+            };
+            try
+            {
+                BaseFunctions functions = new BaseFunctions();
+                response.Data = functions.GetOrdersWithEntityData();
+                response.Message = "Api Successfully triggered.";
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message.ToString();
+
+            }
+            return response;
+        }
+    }
+}
---
+++ Models/BaseFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WebApp1.Models
+{
+    public class BaseFunctions
+    {
+        public string GetOrdersWithEntityData() 
+        {
+            OrdersDbContext dbContext = new OrdersDbContext();
+            List<Order> orders = dbContext.Orders?.ToList();
+            var result = from order in orders
+                         group order by order.CustomerID into groupTable
+                         select new 
+                         {
+                             CustomerID = groupTable.First().CustomerID,
+                             OrderCount = groupTable.Count()
+                         };
+            return JsonConvert.SerializeObject(result.ToList());
+        }
+    }
+}
